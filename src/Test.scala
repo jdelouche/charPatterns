5444ee71432c8ca8843bf6ffbeefca58585e329d
Jean-Pierre Delouche <jdelouche@gmail.com>
2013-11-16 13:38:19 +0100

    double

--- src/Test.scala
+++ src/Test.scala
@@ -1,10 +1,12 @@
 /**
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * Created with IntelliJ IDEA.                                         *
  * User: jdelouche                                                     *
  * Date: 5/16/13                                                       *
  * Time: 1:50 PM                                                       *
  * To change this template use File | Settings | File Templates.       *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  */
 
